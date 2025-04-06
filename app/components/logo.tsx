b81289a7010d18231e3d7a973bc98216294f0fa8
lewjunhong <hong126001@gmail.com>
2025-04-06 16:46:17 +0800

    create sidebar

--- app/components/logo.tsx
+++ app/components/logo.tsx
@@ -3,7 +3,7 @@ import React from 'react'
  const logo = () => {
   return (
     <div className="flex gap-3">
-        <div className="ml-3 text-white flex items-center justify-center w-9 h-9 bg-radial-[at_25%_25%] from-white to-stone-900 to-75% rounded-xl ">
+        <div className="ml-3 text-white flex items-center justify-center w-9 h-9 bg-radial-[at_20%_20%] shadow-lg shadow-[#008f81]/40 from-white to-[#008f81] to-75% rounded-xl ">
             
             <div className="flex flex-col gap-[2px]">
                 <div className="h-[2px] bg-zinc-100 w-3.5 rounded-full shadow-xl"></div>
@@ -12,7 +12,7 @@ import React from 'react'
             </div>
            
         </div>
-        <span className="font-inter font-[600] text-2xl text-zinc-950 relative top-[2px]">Invoize</span>
+        <span className="font-inter font-[600] text-2xl text-[#008f81] relative top-[2px]">Invoizo</span>
     </div>
   )
 }
--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -1,17 +1,47 @@
 import React from 'react'
 import Logo from '@/components/logo';
+import { HugeiconsIcon } from '@hugeicons/react';
+import { Invoice01Icon,DashboardSquare02Icon } from '@hugeicons/core-free-icons';
+import Link from 'next/link';
 const sidebar = () => {
   return (
-    <div className="w-[300px] bg-gray-50 border-r border-gray-200 px-6 py-5">
+    <div className="w-[300px] bg-gray-50 border-r border-gray-200 py-5 px-6">
         <Logo/>
         <div className="mt-5">
-            <div className="items-center rounded-xl bg-white px-3 py-2 shadow-sm flex gap-3">
-                <div className="flex rounded-full items-center justify-center bg-linear-to-r shadow-md from-zinc-600 to-zinc-900 text-white font-inter font-medium px-2 w-8 h-8 text-sm">GF</div>
-                <div className="flex flex-col">
-                    <span className="text-zinc-800 font-medium text-sm font-inter">Greek Freak</span>
+            <div className="items-center rounded-xl bg-white px-3 py-2 border border-gray-200 flex gap-3">
+                <div className="flex rounded-full items-center justify-center bg-linear-to-r shadow-md from-zinc-600 to-zinc-900 text-white font-inter font-medium px-2 w-8 h-8 text-sm">FH</div>
+                <div className="flex flex-col flex-1 overflow-hidden">
+                    <span className="text-zinc-800 font-medium text-sm font-inter truncate">Feng Heng Aluminium And Glass</span>
                     <span className="text-xs text-zinc-400">Business Account</span>
                 </div>
             </div>
+        </div>
+        <div className="py-6 flex flex-col">
+          <span className="font-inter text-xs text-gray-400 font-medium px-1">Main Menu</span>
+          <div className="py-2 flex flex-col *:text-gray-500 *:px-3 *:border gap-y-1 *:border-transparent *:py-3 *:flex *:rounded-lg  *:font-medium text-sm *:items-center *:font-inter *:gap-x-4 *:hover:bg-white *:hover:border-gray-200 *:hover:shadow-sm *:transition-all *:duration-200 *:hover:text-gray-800">
+          <Link href="/">
+              <HugeiconsIcon
+                icon={DashboardSquare02Icon}
+                size={22}
+                color="currentColor"
+                strokeWidth={1.5}
+                className="text-gray-800"
+              />
+              <span>Dashboard</span>
+            </Link>
+            <Link href="/invoice">
+              <HugeiconsIcon
+                icon={Invoice01Icon}
+                size={22}
+                color="currentColor"
+                strokeWidth={1.5}
+                className="text-gray-800"
+              />
+              <span>Invoices</span>
+            </Link>
+          </div>
+       
+         
         </div>
     </div>
   )
